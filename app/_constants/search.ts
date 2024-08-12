1dbfe9e8abd59b1f9633dd8dac847a18374001ec
Delfim Celestino <delfimcelestinoamissepastola@gmail.com>
2024-08-12 13:50:39 +0200

    feat: add BookingItem

---
+++ app/_constants/search.ts
@@ -0,0 +1,27 @@
+interface quickSearchOption {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOptions: quickSearchOption[] = [
+  {
+    imageUrl: "/cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/barba.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/sobrancelha.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "/massagem.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/hidratacao.svg",
+    title: "Hidratação",
+  },
+]
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import { FootprintsIcon, SearchIcon } from "lucide-react"
+import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import ShopItem from "./_components/shop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 const Home = async () => {
   const shops = await db.shop.findMany({})
@@ -34,36 +34,17 @@ const Home = async () => {
 
         {/* BUSCA RAPIDA */}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant={"secondary"}>
-            <Image src={"/cabelo.svg"} width={16} height={16} alt="cabelo" />
-            Cabelo
-          </Button>
-          <Button className="gap-2" variant={"secondary"}>
-            <Image src={"/barba.svg"} width={16} height={16} alt="barba" />
-            Barba
-          </Button>
-          <Button className="gap-2" variant={"secondary"}>
-            <Image
-              src={"/acabamento.svg"}
-              width={16}
-              height={16}
-              alt="acabamento"
-            />
-            Acabamento
-          </Button>
-          <Button className="gap-2" variant={"secondary"}>
-            <Image
-              src={"/sobrancelha.svg"}
-              width={16}
-              height={16}
-              alt="sobrancelha"
-            />
-            Sobrancelha
-          </Button>
-          <Button className="gap-2" variant={"secondary"}>
-            <FootprintsIcon size={16} />
-            Pezinho
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button key={option.title} className="gap-2" variant={"secondary"}>
+              <Image
+                src={option.imageUrl}
+                width={16}
+                height={16}
+                alt="cabelo"
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         {/* 
@@ -78,30 +59,7 @@ IMAGEM */}
         </div>
 
         {/* AGENDAMENTO */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* ESQUERDA */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia Celestino</p>
-              </div>
-            </div>
-            {/* DIREITA */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">12</p>
-              <p className="text-sm">09:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
         {/* RECOMENDADOS */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
