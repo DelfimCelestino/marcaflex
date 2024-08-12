967a10dc6d795804999c04a83d6e80dea62d9094
Delfim Celestino <delfimcelestinoamissepastola@gmail.com>
2024-08-12 13:36:02 +0200

    feat: add quick search

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { SearchIcon } from "lucide-react"
+import { FootprintsIcon, SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
@@ -11,21 +11,61 @@ import ShopItem from "./_components/shop-item"
 
 const Home = async () => {
   const shops = await db.shop.findMany({})
+  const popularShops = await db.shop.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  })
 
   return (
     <div>
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Ola, Delfim!</h2>
+        <h2 className="text-xl font-bold">Olá, Delfim!</h2>
         <p>Segunda-feira, 12 de agosto.</p>
 
         {/* BUSCA */}
         <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Faca sua busca..." />
+          <Input placeholder="Faça sua Busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
+
+        {/* BUSCA RAPIDA */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <Button className="gap-2" variant={"secondary"}>
+            <Image src={"/cabelo.svg"} width={16} height={16} alt="cabelo" />
+            Cabelo
+          </Button>
+          <Button className="gap-2" variant={"secondary"}>
+            <Image src={"/barba.svg"} width={16} height={16} alt="barba" />
+            Barba
+          </Button>
+          <Button className="gap-2" variant={"secondary"}>
+            <Image
+              src={"/acabamento.svg"}
+              width={16}
+              height={16}
+              alt="acabamento"
+            />
+            Acabamento
+          </Button>
+          <Button className="gap-2" variant={"secondary"}>
+            <Image
+              src={"/sobrancelha.svg"}
+              width={16}
+              height={16}
+              alt="sobrancelha"
+            />
+            Sobrancelha
+          </Button>
+          <Button className="gap-2" variant={"secondary"}>
+            <FootprintsIcon size={16} />
+            Pezinho
+          </Button>
+        </div>
+
         {/* 
 IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
@@ -72,7 +112,26 @@ IMAGEM */}
             <ShopItem key={shop.id} shop={shop} />
           ))}
         </div>
+        {/* Populares */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularShops.map((shop) => (
+            <ShopItem key={shop.id} shop={shop} />
+          ))}
+        </div>
       </div>
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-xs text-gray-400">
+              © 2024 Copyright <span className="font-bold">MarcaFlex</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
